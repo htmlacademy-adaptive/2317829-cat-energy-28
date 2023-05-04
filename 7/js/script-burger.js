5c169f21b0e88a2e61879bee871a99d5a976d24e
Keks <keks@htmlacademy.ru>
2023-05-04 14:38:06 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/script-burger.js
@@ -0,0 +1,24 @@
+let toggle = document.querySelector('.toggle-navigation');
+
+toggle.classList.remove('toggle-navigation--no-js');
+
+toggle.addEventListener('click', function () {
+  toggle.classList.toggle('toggle-navigation--burger');
+  toggle.classList.toggle('toggle-navigation--cross');
+});
+
+let mainNavigation = document.querySelector('.main-navigation ');
+let toggleNavigation = document.querySelector('.toggle-navigation');
+
+mainNavigation.classList.remove('main-navigation--no-js');
+
+toggleNavigation.addEventListener('click', function () {
+  if (mainNavigation.classList.contains('main-navigation--closed')) {
+    mainNavigation.classList.remove('main-navigation--closed');
+    mainNavigation.classList.add('main-navigation--opened');
+  } else {
+    mainNavigation.classList.add('main-navigation--closed');
+    mainNavigation.classList.remove('main-navigation--opened');
+  }
+});
+
